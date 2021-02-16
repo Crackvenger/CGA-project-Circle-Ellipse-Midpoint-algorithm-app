06a2b236c28fc63f4a72b156d0c1ba8c414355b2
Crackvenger <tsukinome123@gmail.com>
2021-02-16 17:13:41 +0900

    Add project files.

---
+++ CircleTest/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnCircle = New System.Windows.Forms.Button()
+        Me.pbDrawing = New System.Windows.Forms.PictureBox()
+        Me.btnElipse = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbY = New System.Windows.Forms.TextBox()
+        Me.tbX = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblX = New System.Windows.Forms.Label()
+        Me.lblY = New System.Windows.Forms.Label()
+        Me.tbRadius = New System.Windows.Forms.TextBox()
+        CType(Me.pbDrawing, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCircle
+        '
+        Me.btnCircle.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnCircle.Location = New System.Drawing.Point(1056, 774)
+        Me.btnCircle.Name = "btnCircle"
+        Me.btnCircle.Size = New System.Drawing.Size(136, 60)
+        Me.btnCircle.TabIndex = 0
+        Me.btnCircle.Text = "Circle"
+        Me.btnCircle.UseVisualStyleBackColor = True
+        '
+        'pbDrawing
+        '
+        Me.pbDrawing.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.pbDrawing.BackColor = System.Drawing.Color.White
+        Me.pbDrawing.Location = New System.Drawing.Point(520, 35)
+        Me.pbDrawing.Name = "pbDrawing"
+        Me.pbDrawing.Size = New System.Drawing.Size(1022, 716)
+        Me.pbDrawing.TabIndex = 1
+        Me.pbDrawing.TabStop = False
+        '
+        'btnElipse
+        '
+        Me.btnElipse.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnElipse.Location = New System.Drawing.Point(1259, 774)
+        Me.btnElipse.Name = "btnElipse"
+        Me.btnElipse.Size = New System.Drawing.Size(143, 60)
+        Me.btnElipse.TabIndex = 2
+        Me.btnElipse.Text = "Elipse"
+        Me.btnElipse.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Snow
+        Me.Panel1.Controls.Add(Me.tbRadius)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.tbY)
+        Me.Panel1.Controls.Add(Me.tbX)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(464, 855)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(172, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Radius"
+        '
+        'tbY
+        '
+        Me.tbY.Location = New System.Drawing.Point(68, 108)
+        Me.tbY.Name = "tbY"
+        Me.tbY.Size = New System.Drawing.Size(74, 26)
+        Me.tbY.TabIndex = 3
+        '
+        'tbX
+        '
+        Me.tbX.Location = New System.Drawing.Point(68, 56)
+        Me.tbX.Name = "tbX"
+        Me.tbX.Size = New System.Drawing.Size(74, 26)
+        Me.tbX.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Y"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "X"
+        '
+        'lblX
+        '
+        Me.lblX.AutoSize = True
+        Me.lblX.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblX.Location = New System.Drawing.Point(554, 774)
+        Me.lblX.Name = "lblX"
+        Me.lblX.Size = New System.Drawing.Size(30, 17)
+        Me.lblX.TabIndex = 4
+        Me.lblX.Text = "X - "
+        '
+        'lblY
+        '
+        Me.lblY.AutoSize = True
+        Me.lblY.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblY.Location = New System.Drawing.Point(554, 817)
+        Me.lblY.Name = "lblY"
+        Me.lblY.Size = New System.Drawing.Size(30, 17)
+        Me.lblY.TabIndex = 5
+        Me.lblY.Text = "Y - "
+        '
+        'tbRadius
+        '
+        Me.tbRadius.Location = New System.Drawing.Point(239, 56)
+        Me.tbRadius.Name = "tbRadius"
+        Me.tbRadius.Size = New System.Drawing.Size(74, 26)
+        Me.tbRadius.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.Firebrick
+        Me.ClientSize = New System.Drawing.Size(1597, 855)
+        Me.Controls.Add(Me.lblY)
+        Me.Controls.Add(Me.lblX)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnElipse)
+        Me.Controls.Add(Me.pbDrawing)
+        Me.Controls.Add(Me.btnCircle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbDrawing, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCircle As Button
+    Friend WithEvents pbDrawing As PictureBox
+    Friend WithEvents btnElipse As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents tbY As TextBox
+    Friend WithEvents tbX As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblX As Label
+    Friend WithEvents lblY As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbRadius As TextBox
+End Class
---
+++ CircleTest/Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Drawing
+Public Class Form1
+    Dim radius As Integer
+    Dim x, y, b, fx, fy As Integer
+    Dim d As Long
+    Dim rx_sqr, ry_sqr As Integer
+    Dim rx, ry As Integer
+    Dim midpoint As Integer
+    Dim x_center As Integer
+    Dim y_center As Integer
+
+
+    Dim bm As New Bitmap(1, 1)
+    Dim gfx As Graphics
+
+    Private Sub pbDrawing_MouseClick(sender As Object, e As MouseEventArgs) Handles pbDrawing.MouseClick
+        Dim MouseX As String = e.X
+        Dim MouseY As String = e.Y
+
+        tbX.Text = MouseX
+        tbY.Text = MouseY
+    End Sub
+
+    Private Sub btnElipse_Click(sender As Object, e As EventArgs) Handles btnElipse.Click
+        Call ellipse()
+    End Sub
+
+    Private Sub btnCircle_Click(sender As Object, e As EventArgs) Handles btnCircle.Click
+        Call MidPoint_Circle()
+    End Sub
+
+
+    Private Sub circlePoints()
+        'We initially used small elipses to repesent a pixel..Until we learned how to actually set pixels'
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center + y, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + y, y_center + x, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - y, y_center + x, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center + y, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center - y, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - y, y_center - x, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + y, y_center - x, 10, 10))
+        'gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center - y, 10, 10))
+        gfx.DrawImage(bm, x_center + x, y_center + y, 10, 10)
+        gfx.DrawImage(bm, x_center + y, y_center + x, 10, 10)
+        gfx.DrawImage(bm, x_center - y, y_center + x, 10, 10)
+        gfx.DrawImage(bm, x_center - x, y_center + y, 10, 10)
+        gfx.DrawImage(bm, x_center - x, y_center - y, 10, 10)
+        gfx.DrawImage(bm, x_center - y, y_center - x, 10, 10)
+        gfx.DrawImage(bm, x_center + y, y_center - x, 10, 10)
+        gfx.DrawImage(bm, x_center + x, y_center - y, 10, 10)
+
+    End Sub
+
+    Public Sub MidPoint_Circle(x_center, y_center, radius)
+        gfx = pbDrawing.CreateGraphics
+        bm.SetPixel(0, 0, Color.Red)
+        radius = 100
+        x = 0
+        y = radius
+        x_center = 190
+        y_center = 200
+        d = (5 / 4) - radius
+
+        Do While y > x
+            Call circlePoints()
+            If d < 0 Then
+                d = d + (2 * x) + 1
+            Else
+                d = d + (2 * (x - y)) + 1
+                y = y - 1
+            End If
+            x += 1
+        Loop
+
+    End Sub
+
+    Public Sub Midpoint_Elipse()
+        rx = 20
+        ry = 30
+        x = 0
+        y = ry
+        rx_sqr = rx * rx
+        ry_sqr = ry * ry
+        x_center = pbDrawing.Width / 2
+        y_center = pbDrawing.Height / 2
+        fx = ry_sqr * (2 * y - 1)
+        fy = 2 * rx_sqr * (x + 1)
+        d = 4 * ry_sqr - 4 * rx_sqr * ry + rx_sqr
+        'this part is for region 1'
+        Do While (2 * rx_sqr * (x + 1)) < (ry_sqr * (2 * y - 1))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center + y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center - y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center - y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center + y, 10, 10))
+            'putpixel(xc + x, yc - y, WHITE);
+            'putpixel(xc - x, yc + y, WHITE);
+            'putpixel(xc + x, yc + y, WHITE);
+            'putpixel(xc - x, yc - y, WHITE);
+            If d > 0 Then
+                y -= 1
+                d += ry_sqr * (8 * x + 12) + rx_sqr * (8 - 8 * y)
+            Else
+                d += ry_sqr * (8 * x + 12)
+            End If
+            x += 1
+        Loop
+
+        'this part is for region 2'
+        d = ry_sqr * ((2 * x + 1) * (2 * x + 1)) + 4 * rx_sqr * ((y - 1) * (y - 1)) - 4 * rx_sqr * ry_sqr
+        Do While y > 0
+
+            If d < 0 Then
+                x += 1
+                d = d + ry_sqr * (8 * x + 8) + rx_sqr * (12 - 8 * y)
+            Else
+                d = d + rx_sqr * (12 - 8 * y)
+            End If
+            y -= 1
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center + y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center - y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center + x, y_center - y, 10, 10))
+            gfx.DrawEllipse(Pens.Red, New Rectangle(x_center - x, y_center + y, 10, 10))
+        Loop
+    End Sub
+
+    'New Elipse code made made by Indhira'
+    Public Sub ellipse()
+
+        'Dim gEllipse As Graphics = pbDrawing.CreateGraphics
+        gfx = pbDrawing.CreateGraphics
+        'Dim imgEllipse As New Bitmap(1, 1)
+        bm.SetPixel(0, 0, Color.Indigo)
+
+
+        Dim xc As Integer = 100
+        Dim yc As Integer = 100
+        Dim a As Integer = 75
+        Dim b As Integer = 25
+
+        Dim d As Long
+
+        Dim x = 0
+        Dim y = b 'y=b
+
+        Dim a_square = a * a
+        Dim b_square = b * b
+
+        d = 4 * b_square - 4 * a_square * b + a_square
+
+        Do
+            gfx.DrawImage(bm, x + xc, y + yc, 14, 14)
+            gfx.DrawImage(bm, -x + xc, y + yc, 14, 14)
+            gfx.DrawImage(bm, x + xc, -y + yc, 14, 14)
+            gfx.DrawImage(bm, -x + xc, -y + yc, 14, 14)
+
+            If d > 0 Then
+                y = y - 1
+                d = d + b_square * (8 * x + 12) + a_square * (8 - 8 * y)
+            Else
+                d = d + b_square * (8 * x + 12)
+            End If
+
+            x = x + 1
+
+
+        Loop Until 2 * b_square * (x + 1) >= a_square * (2 * y - 1)
+
+        'region 2
+        d = b_square * ((2 * x + 1) * (2 * x + 1)) + 4 * a_square * ((y - 1) * (y - 1)) - 4 * a_square * b_square
+
+        While y > 0
+
+
+            If d < 0 Then
+                x = x + 1
+                d = d + b_square * (8 * x + 8) + a_square * (12 - 8 * y)
+            Else
+                d = d + a_square * (12 - 8 * y)
+            End If
+            y = y - 1
+
+            gfx.DrawImage(bm, x + xc, y + yc, 14, 14)
+            gfx.DrawImage(bm, -x + xc, y + yc, 14, 14)
+            gfx.DrawImage(bm, x + xc, -y + yc, 14, 14)
+            gfx.DrawImage(bm, -x + xc, -y + yc, 14, 14)
+
+
+        End While
+    End Sub
+
+    Private Sub pbDrawing_MouseMove(sender As Object, e As MouseEventArgs) Handles pbDrawing.MouseMove
+        Dim MouseX As String = e.X.ToString
+        Dim MouseY As String = e.Y.ToString
+
+        lblX.Text = "X - " + MouseX
+        lblY.Text = "Y - " + MouseY
+
+
+    End Sub
+
+
+End Class
